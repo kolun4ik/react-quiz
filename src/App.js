9a97f6564ab7be60d461aebde141387b424f5cc8
kolun4ik <11_ka@rambler.ru>
2020-01-03 17:06:44 +0300

    Сформирована структура приложения

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <Quiz/>
+            </Layout>
+        )
+    }
+}
+export default App;
