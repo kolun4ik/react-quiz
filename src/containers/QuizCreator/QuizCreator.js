12f8b1a8897c94d0036112233304e1571b3ae2f1
kolun4ik <11_ka@rambler.ru>
2020-01-05 20:14:59 +0300

    Настроены роуты и добавленны доп страницы: Auth, QuizCreator, QuizList

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class QuizCreator extends Component {
+    render() {
+        return (
+            <div>
+                <h1>QuitCreator</h1>
+            </div>
+        );
+    }
+}
+
+export default QuizCreator;
